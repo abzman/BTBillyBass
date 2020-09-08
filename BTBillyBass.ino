c309fe6d7ea40adc543aea69f8c91d04068bcc66
abzman <abzman2000@gmail.com>
2020-09-08 02:19:52 -0400

    Update BTBillyBass.ino
    changes for my slightly custom three motor fish

--- BTBillyBass.ino
+++ BTBillyBass.ino
@@ -1,39 +1,50 @@
 /*This is my crack at a state-based approach to automating a Big Mouth Billy Bass.
- This code was built on work done by both Donald Bell and github user jswett77. 
- See links below for more information on their previous work.
+  This code was built on work done by both Donald Bell and github user jswett77.
+  See links below for more information on their previous work.
 
- In this code you'll find reference to the MX1508 library, which is a simple 
- library I wrote to interface with the extremely cheap 2-channel H-bridges that
- use the MX1508 driver chip. It may also work with other H-bridges that use different
- chips (such as the L298N), so long as you can PWM the inputs.
+  In this code you'll find reference to the MX1508 library, which is a simple
+  library I wrote to interface with the extremely cheap 2-channel H-bridges that
+  use the MX1508 driver chip. It may also work with other H-bridges that use different
+  chips (such as the L298N), so long as you can PWM the inputs.
 
- This code watches for a voltage increase on input A0, and when sound rises above a
- set threshold it opens the mouth of the fish. When the voltage falls below the threshold,
- the mouth closes.The result is the appearance of the mouth "riding the wave" of audio
- amplitude, and reacting to each voltage spike by opening again. There is also some code
- which adds body movements for a bit more personality while talking.
+  This code watches for a voltage increase on input A0, and when sound rises above a
+  set threshold it opens the mouth of the fish. When the voltage falls below the threshold,
+  the mouth closes.The result is the appearance of the mouth "riding the wave" of audio
+  amplitude, and reacting to each voltage spike by opening again. There is also some code
+  which adds body movements for a bit more personality while talking.
 
- Most of this work was based on the code written by jswett77, and can be found here:
- https://github.com/jswett77/big_mouth/blob/master/billy.ino
+  Most of this work was based on the code written by jswett77, and can be found here:
+  https://github.com/jswett77/big_mouth/blob/master/billy.ino
 
- Donald Bell wrote the initial code for getting a Billy Bass to react to audio input,
- and his project can be found on Instructables here:
- https://www.instructables.com/id/Animate-a-Billy-Bass-Mouth-With-Any-Audio-Source/
+  Donald Bell wrote the initial code for getting a Billy Bass to react to audio input,
+  and his project can be found on Instructables here:
+  https://www.instructables.com/id/Animate-a-Billy-Bass-Mouth-With-Any-Audio-Source/
 
- Author: Jordan Bunker <jordan@hierotechnics.com> 2019
- License: MIT License (https://opensource.org/licenses/MIT)
+  Author: Jordan Bunker <jordan@hierotechnics.com> 2019
+  License: MIT License (https://opensource.org/licenses/MIT)
+
+  Revised: Evan Allen <abzman2000@gmail.com> September 2020
+  Modified code for original three motor billy bass and tuned for my set of 
+  strange drive voltages and slightly old and broken motors
 */
 
 #include <MX1508.h>
 
-MX1508 bodyMotor(6, 9); // Sets up an MX1508 controlled motor on PWM pins 6 and 9
+
+MX1508 tailMotor(9, 6); // Sets up an MX1508 controlled motor on PWM pins 9 and 6
+MX1508 bodyMotor(10, 11); // Sets up an MX1508 controlled motor on PWM pins 10 and 11
 MX1508 mouthMotor(5, 3); // Sets up an MX1508 controlled motor on PWM pins 5 and 3
 
-int soundPin = A0; // Sound input
+//TODO do something with light
 
-int silence = 12; // Threshold for "silence". Anything below this level is ignored.
+int lightPin = A0; // Light input
+int soundPin = A1; // Sound input
+int buttonPin = 12; // button input
+
+int silence = 10; // Threshold for "silence". Anything below this level is ignored.
 int bodySpeed = 0; // body motor speed initialized to 0
 int soundVolume = 0; // variable to hold the analog audio value
+int lightIntensity = 0; // variable to hold the analog light value
 int fishState = 0; // variable to indicate the state Billy is in
 
 bool talking = false; //indicates whether the fish should be talking or not
@@ -45,20 +56,39 @@ long bodyActionTime;
 long lastActionTime;
 
 void setup() {
-//make sure both motor speeds are set to zero
-  bodyMotor.setSpeed(0); 
+  //make sure both motor speeds are set to zero
+  bodyMotor.setSpeed(0);
   mouthMotor.setSpeed(0);
+  tailMotor.setSpeed(0);
 
-//input mode for sound pin
+  //input mode for sound pin
   pinMode(soundPin, INPUT);
+  pinMode(lightPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
 
-  Serial.begin(9600);
+  //Serial.begin(115200);
 }
 
 void loop() {
   currentTime = millis(); //updates the time each time the loop is run
   updateSoundInput(); //updates the volume level detected
+  updateLightInput(); //updates the light level detected
+  /*if(soundVolume > silence)
+    {
+    Serial.print("light: ");
+    Serial.print(lightIntensity);
+    Serial.print(", sound: ");
+    Serial.println(soundVolume);
+    }*/
   SMBillyBass(); //this is the switch/case statement to control the state of the fish
+  if(digitalRead(buttonPin) == 0)
+    {
+    //openMouth();
+    flap();
+    //bodyTest();
+    }else{
+    //closeMouth();
+    }
 }
 
 void SMBillyBass() {
@@ -72,7 +102,7 @@ void SMBillyBass() {
         }
       } else if (currentTime > mouthActionTime + 100) { //if we're beyond the scheduled talking time, halt the motors
         bodyMotor.halt();
-        mouthMotor.halt();
+        closeMouth();
       }
       if (currentTime - lastActionTime > 1500) { //if Billy hasn't done anything in a while, we need to show he's bored
         lastActionTime = currentTime + floor(random(30, 60)) * 1000L; //you can adjust the numbers here to change how often he flaps
@@ -108,16 +138,23 @@ int updateSoundInput() {
   soundVolume = analogRead(soundPin);
 }
 
+int updateLightInput() {
+  lightIntensity = analogRead(lightPin);
+}
+
 void openMouth() {
   mouthMotor.halt(); //stop the mouth motor
-  mouthMotor.setSpeed(220); //set the mouth motor speed
+  mouthMotor.setSpeed(250); //set the mouth motor speed
   mouthMotor.forward(); //open the mouth
 }
 
 void closeMouth() {
   mouthMotor.halt(); //stop the mouth motor
-  mouthMotor.setSpeed(180); //set the mouth motor speed
+  // original billy bass mouth spring loaded
+  mouthMotor.setSpeed(80); //set the mouth motor speed
   mouthMotor.backward(); // close the mouth
+  delay(20);
+  mouthMotor.halt(); //stop the mouth motor
 }
 
 void articulateBody(bool talking) { //function for articulating the body
@@ -130,20 +167,22 @@ void articulateBody(bool talking) { //function for articulating the body
         bodyMotor.forward(); //move the body motor to raise the head
 
       } else if (r < 3) {
-        bodySpeed = 150; //move the body slowly
+        bodySpeed = 190; //move the body slowly
         bodyActionTime = currentTime + floor(random(500, 1000)); //schedule body action for .5 to 1 seconds from current time
         bodyMotor.forward(); //move the body motor to raise the head
 
       } else if (r == 4) {
-        bodySpeed = 200;  // move the body medium speed
+        bodySpeed = 230;  // move the body medium speed
         bodyActionTime = currentTime + floor(random(500, 1000)); //schedule body action for .5 to 1 seconds from current time
         bodyMotor.forward(); //move the body motor to raise the head
 
       } else if ( r == 5 ) {
         bodySpeed = 0; //set body motor speed to 0
         bodyMotor.halt(); //stop the body motor (to keep from violent sudden direction changes)
-        bodyMotor.setSpeed(255); //set the body motor to full speed
-        bodyMotor.backward(); //move the body motor to raise the tail
+        tailMotor.halt(); //halt the motor
+        tailMotor.setSpeed(255); //set the tail motor to full speed
+        tailMotor.backward(); //move the tail motor to raise the tail
+
         bodyActionTime = currentTime + floor(random(900, 1200)); //schedule body action for .9 to 1.2 seconds from current time
       }
       else {
@@ -157,6 +196,7 @@ void articulateBody(bool talking) { //function for articulating the body
   } else {
     if (currentTime > bodyActionTime) { //if we're beyond the scheduled body action time
       bodyMotor.halt(); //stop the body motor
+      tailMotor.halt(); //halt the motor
       bodyActionTime = currentTime + floor(random(20, 50)); //set the next scheduled body action to current time plus .02 to .05 seconds
     }
   }
@@ -164,8 +204,15 @@ void articulateBody(bool talking) { //function for articulating the body
 
 
 void flap() {
+  tailMotor.setSpeed(180); //set the body motor to full speed
+  tailMotor.backward(); //move the body motor to raise the tail
+  delay(500); //wait a bit, for dramatic effect
+  tailMotor.halt(); //halt the motor
+}
+
+void bodyTest() {
   bodyMotor.setSpeed(180); //set the body motor to full speed
-  bodyMotor.backward(); //move the body motor to raise the tail
+  bodyMotor.forward(); //move the body motor to raise the tail
   delay(500); //wait a bit, for dramatic effect
   bodyMotor.halt(); //halt the motor
 }
